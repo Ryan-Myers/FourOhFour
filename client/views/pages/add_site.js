99732209a3288afc52477e197031d4b8994f91a3
Ryan Myers <ryan@ryanmyers.ca>
2014-06-15 14:58:58 -0400

    Got the add_site page working. No feedback however.

--- client/views/pages/add_site.js
+++ client/views/pages/add_site.js
@@ -2,11 +2,13 @@ Template.addSite.events({
   'submit form' : function(e) {
     e.preventDefault();
     
+    var siteName = $(e.target).find('[name=siteName]').val();
+    
     var site = {
-      apiKey: '',
-      ownerId: '',
-      siteName: $(e.target).find('[name=siteName]').val(),
-      errors: []
+      'apiKey': Random.id(),
+      'ownerId': Meteor.userId(),
+      'siteName': siteName,
+      'errors': []
     };
     
     Site.insert(site);
