6c297962fc3fe10f9c720527727949ee11770274
Ryan Myers <ryan@ryanmyers.ca>
2014-06-15 15:04:49 -0400

    Remove unused crypto packages.

--- client/views/pages/add_site.js
+++ client/views/pages/add_site.js
@@ -2,12 +2,10 @@ Template.addSite.events({
   'submit form' : function(e) {
     e.preventDefault();
     
-    var siteName = $(e.target).find('[name=siteName]').val();
-    
     var site = {
       'apiKey': Random.id(),
       'ownerId': Meteor.userId(),
-      'siteName': siteName,
+      'siteName': $(e.target).find('[name=siteName]').val(),
       'errors': []
     };
     
